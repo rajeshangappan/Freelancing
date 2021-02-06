c39f0d04b63cef564e2a9434af39d06143acb955
ragavancse <ragavancse@gmail.com>
2021-02-06 20:44:49 +0530

    Store management basic UI

---
+++ StoreManagement/XamSample/XamSample/XamSample/Models/Request/SyncRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace XamSample.Models
+{
+    /// <summary>
+    /// Defines the <see cref="SyncRequest" />.
+    /// </summary>
+    public class SyncRequest
+    {
+        #region PUBLIC_PPTY
+
+        /// <summary>
+        /// Gets or sets the OfflineVersion.
+        /// </summary>
+        public long OfflineVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Products.
+        /// </summary>
+        public List<Product> Products { get; set; }
+
+        #endregion
+    }
+}
